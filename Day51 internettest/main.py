ca8662e7c8c7a23d309839100cfe6c3e186cc05b
walukaui <cwaluka@yahoo.com>
2024-06-18 11:47:51 -0500

    "Day 51 starting files"

--- Day51 internettest/main.py
+++ Day51 internettest/main.py
@@ -1,1 +1,19 @@
-import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from time import sleep
+
+# keep browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.speedtest.net/")
+sleep(2)
+
+driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]').click()
+
+
+
