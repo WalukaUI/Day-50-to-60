efe860630a6be7670344e16cafc77aefcd5b1038
walukaui <cwaluka@yahoo.com>
2024-06-24 16:37:11 -0500

    "compleated project day 53"

--- Day53 capstone project/main.py
+++ Day53 capstone project/main.py
@@ -15,22 +15,35 @@ web_page = res.text
 soup = BeautifulSoup(web_page, "html.parser")
 posts = soup.find_all(name="li", class_="ListItem-c11n-8-84-3-StyledListCardWrapper")
 
-# for post in  posts:
-#     address = post.find(name="address").text
-#     correct_address = address.strip()
-#     correct_href = post.find(name="a", class_="StyledPropertyCardDataArea-anchor", href=True)['href']
-#     price = post.find(name="span", class_="PropertyCardWrapper__StyledPriceLine").text
-#     correct_price = "".join([num for num in price.split(" ")[0] if num.isdigit()])
-
 # Keep browser open so you can manually log out
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+# chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(google_sheet)
-time.sleep(1)
-inputs = driver.find_elements(by=By.CLASS_NAME, value="whsOnd")
-for data_input in inputs:
-    data_input.send_keys("name")
+for post in posts:
+    data = []
+    address = post.find(name="address").text
+    correct_address = address.strip()
+    data.append(correct_address)
+
+    price = post.find(name="span", class_="PropertyCardWrapper__StyledPriceLine").text
+    correct_price = "".join([num for num in price.split(" ")[0] if num.isdigit()])
+    data.append(correct_price)
+
+    correct_href = post.find(name="a", class_="StyledPropertyCardDataArea-anchor", href=True)['href']
+    data.append(correct_href)
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(google_sheet)
+
+    btn = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+    inputs = driver.find_elements(by=By.CLASS_NAME, value="whsOnd")
+    time.sleep(0.1)
+    inputs[0].send_keys(correct_address)
+    inputs[1].send_keys(f"${correct_price}")
+    inputs[2].send_keys(correct_href)
+    btn.click()
+
+
+
 
 
