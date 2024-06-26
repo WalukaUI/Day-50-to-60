f2d4d6c39ed169d6c22c4eb1359c8b2e27c9d117
walukaui <cwaluka@yahoo.com>
2024-06-26 17:23:03 -0500

    "day 54 and 55 merged"

--- Day54 newproject/main.py
+++ Day54 newproject/main.py
@@ -2,6 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "Hello, World! fghjfgjgfjgfjgfjgfjgf"
+
+
+@app.route("/username/<name>/<int:number>")
+def bye(name, number):
+    return f"helloooooohhh {name}, number is {number}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
