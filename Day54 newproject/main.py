c95ae0dc2d68613dd23a86f4eed20b6133018b72
walukaui <cwaluka@yahoo.com>
2024-06-30 15:22:50 -0500

    "added decorative functions"

--- Day54 newproject/main.py
+++ Day54 newproject/main.py
@@ -3,14 +3,32 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+def make_bold(fn):
+    def wrapper():
+        return f'<b>{fn()}</b>'
+    return wrapper
+
+
+def make_italic(fn):
+    def wrapper():
+        return f'<i>{fn()}</i>'
+    return wrapper
+
+
+@app.route("/bye")
+@make_bold
+@make_italic
 def hello_world():
     return "Hello, World! fghjfgjgfjgfjgfjgfjgf"
+
+@app.route("/name")
+def hello_name():
+    return "name is name"
 
 
 @app.route("/username/<name>/<int:number>")
 def bye(name, number):
-    return f"helloooooohhh {name}, number is {number}"
+    return f"helloooooohhhh {name}, number is {number}"
 
 
 if __name__ == "__main__":
