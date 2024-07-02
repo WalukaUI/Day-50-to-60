450b81fe1c4fe68ca03b7b69e4618a53692108fe
walukaui <cwaluka@yahoo.com>
2024-07-02 17:15:40 -0500

    "Day 57 compleated files with jinja if else statement with sigle post "

--- Day57 blog/server.py
+++ Day57 blog/server.py
@@ -11,6 +11,13 @@ def hello_world():
     return render_template("blogs.html", all_posts=posts)
 
 
+@app.route("/posts/<num>")
+def get_anchor(num):
+    response = requests.get(f"https://api.npoint.io/c790b4d5cab58020d391/{num}")
+    post = response.json()
+    return render_template("blog.html", post=post["title"], sub=post["subtitle"])
+
+
 @app.route("/guess/<name>")
 def hello_name(name):
     ages = requests.get(f"https://api.agify.io/?name={name}")
