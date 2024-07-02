3f5c95d4623af4e33d4d4584f83c5cb2486c3253
walukaui <cwaluka@yahoo.com>
2024-07-02 16:41:09 -0500

    "Day 57 compleated files"

--- Day57 blog/server.py
+++ Day57 blog/server.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+
+@app.route("/<name>")
+def hello_name(name):
+    ages = requests.get(f"https://api.agify.io/?name={name}")
+    res = ages.json()
+    return render_template("index.html", name=name, age=res["age"])
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
