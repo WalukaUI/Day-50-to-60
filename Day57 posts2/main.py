6797de2c4d6e4d07853cbaada5ef723445bc9d99
walukaui <cwaluka@yahoo.com>
2024-07-03 13:54:57 -0500

    "day 57 Posts2 compleated project"

--- Day57 posts2/main.py
+++ Day57 posts2/main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from file import jsons
 from post import Post
 
-
 post_objects = []
 for post in jsons:
     post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
@@ -11,10 +10,20 @@ print(post_objects)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    print(post_objects)
     return render_template("index.html", all_posts=post_objects)
+
+
+@app.route("/<int:index>")
+def get_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    print(requested_post)
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
