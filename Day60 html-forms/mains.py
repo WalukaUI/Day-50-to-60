9f25aa87a36c34b451c6fbbbf9ad3b57b9348be8
walukaui <cwaluka@yahoo.com>
2024-07-08 09:21:58 -0500

    "day 60 compleated project"

--- Day60 html-forms/mains.py
+++ Day60 html-forms/mains.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
@@ -16,4 +17,4 @@ def post_data():
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
