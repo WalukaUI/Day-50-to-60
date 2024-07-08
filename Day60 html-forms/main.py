4f2dab190af95e3267460dc3a23b0be03f87058e
walukaui <cwaluka@yahoo.com>
2024-07-08 08:02:14 -0500

    "day 60 passed form data to main.py"

--- Day60 html-forms/main.py
+++ Day60 html-forms/main.py
@@ -1,11 +1,18 @@
-# import requests
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def post_data():
+    name = request.form["fname"]
+    pw = request.form["pword"]
+    return f"<h1>name : {name}, pw : {pw}</h1>"
 
 
 if __name__ == "__main__":
