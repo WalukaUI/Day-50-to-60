bad68302a11e6bee3653645f4d889915b0fe0eaf
walukaui <cwaluka@yahoo.com>
2024-07-08 10:44:56 -0500

    "day 60 compleated project with email feature"

--- Day60 html-forms/main.py
+++ Day60 html-forms/main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
-import requests
+import requests, smtplib
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-
+OWN_PASSWORD = "y"
+OWN_EMAIL = "w"
 app = Flask(__name__)
 
 
@@ -21,12 +22,17 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 @app.route("/post/<int:index>")
@@ -36,7 +42,6 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
-
 
 
 if __name__ == "__main__":
